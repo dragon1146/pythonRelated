ae2e2fe5a235b2175bf310a080b2fd5b6e5acb42
glitch <saniltison@gmail.com>
2020-03-17 09:21:12 -0700

    - starting creating the module to insert new members into the DB - the style will continue to creating modules for every transaction that will take place at a bank

--- bankApp/bankAppDB.py
+++ bankApp/bankAppDB.py
@@ -10,40 +10,26 @@ class Member:
 		self.bal = initdeposit + 5
         # print("{}" "*5 {}" "*5 {}".format("First Name", "Last Name","Account Balance"))
         # print("{}" "*5 {}" "*5 {}{}\n".format(member01.fname, member01.lname,"$", member01.bal))
-		
-	def deposit(self, amtdeposit):
-		self.bal = self.bal + amtdeposit
-        # print("{}" "*5 {}" "*5 {}".format("First Name", "Last Name","Account Balance"))
-        # print("{}" "*5 {}" "*5 {}{}\n".format(member01.fname, member01.lname,"$", member01.bal))
-		
-	def withdraw (self, amtwithdraw):
-		if amtwithdraw > self.bal:
-		    print("Not enough money in account for transaction")
-            # print("{}" "*5 {}" "*5 {}".format("First Name", "Last Name","Account Balance"))
-		else:
-			self.bal = self.bal - amtwithdraw
-        # print("{}" "*5 {}" "*5 {}".format("First Name", "Last Name","Account Balance"))
-        # print("{}" "*5 {}" "*5 {}{}\n".format(member01.fname, member01.lname,"$", member01.bal))
 
-
-def createDB(dbName):
-    conn = sqlite3.connect(dbName)
-
+# THIS BOC WILL IS A FUNC THAT WILL CONX TO AN EXISTING DB AND INSERT A NEW MEMBER INTO THAT DB WITH THE
+# THE ATTRIBUTES OF A NEW MEMBER OBJ.
+# THE OBJ WILL BE CREATED FROM INPUT FROM THE USER
+def addMemToDB(fname, lname, initdeposi):
+    conn = sqlite3.connect("bankAppdb.db")
     c = conn.cursor()
+    # THIS BOC WILL INSERT THE NEW MEMBERS WITH THEIR ATTRIBUTES IN TO THE TABLE CALLED MEMBERS
+    c.execute("INSERT INTO Members VALUES (:first, :last, :bal)", {'first':mem01.fname, 'last':mem01.lname, 'bal':mem01.bal})
 
-    c.execute("""CREATE TABLE members(
-    fname text,
-    lname text, 
-    bal integer   
-    )""")
-# THIS BOC WILL CREATE 2 MEMBER OBJS WITH THE FOLLOWING ATTRIBUTES
-# THIS PROCESS IS CURRENTLY DONE WITH STATIC ENTRIES BUT WILL BE CHANGED TO 
-# DYNAMIC ENTRIES PROVIDED BY THE USER 
-
+    # THIS BOC WILL COMMIT CHANGES MADE TO THE DB THEN CLOSE THE DB
+    conn.commit()    
+    conn.close()
+		
+# THIS BOC WILL REQUEST INFO FOR A NEW MEMBER THEN CREATE A NEW MEM OBJ BASED ON THAT INFO
+# THAT INFO WILL BE INSERTED IN THE MEMBER TABLE IN THE BANKAPP DB BY A FUNC CALLED addMemToDB
 print("Enter the name of the database")
 print()
 dbName = input(">")
-createDB(dbName)
+
 
 print("Enter member's first name")
 print()
@@ -57,13 +43,62 @@ print("Enter member's initial deposit")
 print() 
 initdeposit = int(input(">"))	
 mem01 = Member(fname,lname, initdeposit)
+
+
+
+# def deposit(mem, amtdeposit):
+#     self.bal = self.bal + amtdeposit
+#     # print("{}" "*5 {}" "*5 {}".format("First Name", "Last Name","Account Balance"))
+#     # print("{}" "*5 {}" "*5 {}{}\n".format(member01.fname, member01.lname,"$", member01.bal))
+    
+# def withdraw (self, amtwithdraw):
+#     if amtwithdraw > self.bal:
+#         print("Not enough money in account for transaction")
+#         # print("{}" "*5 {}" "*5 {}".format("First Name", "Last Name","Account Balance"))
+#     else:
+#         self.bal = self.bal - amtwithdraw
+#     # print("{}" "*5 {}" "*5 {}".format("First Name", "Last Name","Account Balance"))
+#     # print("{}" "*5 {}" "*5 {}{}\n".format(member01.fname, member01.lname,"$", member01.bal))
+
+
+# def createDB(dbName):
+    conn = sqlite3.connect(dbName)
+
+    c = conn.cursor()
+
+#     c.execute("""CREATE TABLE members(
+#     fname text,
+#     lname text, 
+#     bal integer   
+#     )""")
+# THIS BOC WILL CREATE 2 MEMBER OBJS WITH THE FOLLOWING ATTRIBUTES
+# THIS PROCESS IS CURRENTLY DONE WITH STATIC ENTRIES BUT WILL BE CHANGED TO 
+# DYNAMIC ENTRIES PROVIDED BY THE USER 
+
+# print("Enter the name of the database")
+# print()
+# dbName = input(">")
+# createDB(dbName)
+
+# print("Enter member's first name")
+# print()
+# fname = input(">")	
+
+# print("Enter member's last name")
+# print()
+# lname = input(">")
+
+# print("Enter member's initial deposit")
+# print() 
+# initdeposit = int(input(">"))	
+# mem01 = Member(fname,lname, initdeposit)
 # mem02 = Member('bob','smith', 2)
 
-conn = sqlite3.connect(dbName)
+# conn = sqlite3.connect(dbName)
 
-c = conn.cursor()
-# THIS BOC WILL INSERT THE NEW MEMBERS WITH THEIR ATTRIBUTES IN TO THE TABLE CALLED MEMBERS
-c.execute("INSERT INTO Members VALUES (:first, :last, :bal)", {'first':mem01.fname, 'last':mem01.lname, 'bal':mem01.bal})
+# c = conn.cursor()
+# # THIS BOC WILL INSERT THE NEW MEMBERS WITH THEIR ATTRIBUTES IN TO THE TABLE CALLED MEMBERS
+# c.execute("INSERT INTO Members VALUES (:first, :last, :bal)", {'first':mem01.fname, 'last':mem01.lname, 'bal':mem01.bal})
 # c.execute("INSERT INTO Members VALUES (:first, :last, :bal)", {'first':mem02.fname, 'last':mem02.lname, 'bal':mem02.bal})
 
 
@@ -89,6 +124,6 @@ WHERE fname = :fname AND lname = :lname""",
 c.execute("SELECT * FROM members WHERE fname = :fname", {'fname': mem01.fname})
 print(c.fetchall())
 
-conn.commit()
+# conn.commit()
 
-conn.close()
+# conn.close()
